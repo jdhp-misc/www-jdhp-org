9f56150b644eeab8485f3af66bbfc46c3543d754
Jeremie DECOCK <jd.jdhp@gmail.com>
2014-10-21 15:07:12 +0200

    Add some slides.

--- jdhp_v2_xslt/push.sh
+++ jdhp_v2_xslt/push.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-scp -r www/* gremy@ssh.tuxfamily.org:~/jdhp/jdhp.org-web/htdocs/
+./clean.sh
+./build.sh
+
+#scp -r www/* gremy@ssh.tuxfamily.org:~/jdhp/jdhp.org-web/htdocs/
+
+rsync -a -v -e ssh --delete www/* gremy@ssh.tuxfamily.org:~/jdhp/jdhp.org-web/htdocs/
+
