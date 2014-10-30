5f9f432e3c135b4596bc3937e4df92c6a91bf958
Jeremie DECOCK <jd.jdhp@gmail.com>
2014-10-30 17:56:32 +0100

    The script can be called from everywhere now.

--- files_download.sh
+++ files_download.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-rsync -a -v -e ssh --delete gremy@ssh.tuxfamily.org:/home/jdhp/jdhp-repository/ ./files/
+JDHP_LOCAL_PATH=~/git/pub/jdhp
+
+rsync -a -v -e ssh --delete gremy@ssh.tuxfamily.org:/home/jdhp/jdhp-repository/ ${JDHP_LOCAL_PATH}/files/
