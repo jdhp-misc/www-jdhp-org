b9be50ad32724c498a490ff0f85bf72c89c100f6
Jeremie DECOCK <jd.jdhp@gmail.com>
2015-06-15 21:55:07 +0200

    Update a path.

--- jdhp_v2_xslt/sync_hevea.sh
+++ jdhp_v2_xslt/sync_hevea.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-JDHP_LOCAL_PATH=~/git/pub/jdhp/jdhp_v2_xslt
+JDHP_LOCAL_PATH=~/git/pub_projects/jdhp/jdhp_v2_xslt
 
 rsync -a -v -e ssh --delete ${JDHP_LOCAL_PATH}/hevea/* gremy@ssh.tuxfamily.org:~/jdhp/jdhp.org-web/htdocs/hevea/
 
