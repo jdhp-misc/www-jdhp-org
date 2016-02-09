5ddf7b3e9a4aafb7190ebff13713fe3bc7cdbf00
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-02-09 17:36:51 +0100

    Misc updates.

--- jdhp_v2_xslt/push.sh
+++ jdhp_v2_xslt/push.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
+DST="tf:~/jdhp/jdhp.org-web/htdocs"
+
 ./clean.sh
 ./build.sh
 
-#scp -r www/* gremy@ssh.tuxfamily.org:~/jdhp/jdhp.org-web/htdocs/
-
-rsync -a -v -e ssh --delete www/* gremy@ssh.tuxfamily.org:~/jdhp/jdhp.org-web/htdocs/
+rsync -a -v -e ssh --delete www/* ${DST}/
 
 ./sync_hevea.sh
 
--- jdhp_v2_xslt/sync_hevea.sh
+++ jdhp_v2_xslt/sync_hevea.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+DST="tf:~/jdhp/jdhp.org-web/htdocs/hevea"
 JDHP_LOCAL_PATH=~/git/pub/projects/jdhp/jdhp_v2_xslt
 
-rsync -a -v -e ssh --delete ${JDHP_LOCAL_PATH}/hevea/* gremy@ssh.tuxfamily.org:~/jdhp/jdhp.org-web/htdocs/hevea/
+rsync -a -v -e ssh --delete ${JDHP_LOCAL_PATH}/hevea/* ${DST}/
 
