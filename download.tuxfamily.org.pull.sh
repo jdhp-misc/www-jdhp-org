5181ea8db5bf1975eabc2a1b6bedee6d7f8dffc6
Jérémie Decock <jd.jdhp@gmail.com>
2016-04-18 16:45:51 +0200

    Keep remote files by default (safer copy) + misc improvements.

--- download.tuxfamily.org.pull.sh
+++ download.tuxfamily.org.pull.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+if [ -z ${JDHP_ROOT_PATH} ] || [ \! -d ${JDHP_ROOT_PATH} ]
+then
+    echo "ERROR: the JDHP_ROOT_PATH environment variable is not set or not valid."
+    exit 1
+fi
+
 LOCAL_DIR_PATH=${JDHP_ROOT_PATH}/download.tuxfamily.org
 REMOTE_DIR_PATH="tf:~/jdhp/jdhp-repository"
 
-rsync -a -v -e ssh --delete ${REMOTE_DIR_PATH}/ ${LOCAL_DIR_PATH}/
+#rsync -a -v -e ssh --delete ${REMOTE_DIR_PATH}/ ${LOCAL_DIR_PATH}/
+rsync -a -v -e ssh ${REMOTE_DIR_PATH}/ ${LOCAL_DIR_PATH}/
--- download.tuxfamily.org.push.sh
+++ download.tuxfamily.org.push.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
+if [ -z ${JDHP_ROOT_PATH} ] || [ \! -d ${JDHP_ROOT_PATH} ]
+then
+    echo "ERROR: the JDHP_ROOT_PATH environment variable is not set or not valid."
+    exit 1
+fi
+
 LOCAL_DIR_PATH=${JDHP_ROOT_PATH}/download.tuxfamily.org
 REMOTE_DIR_PATH="tf:~/jdhp/jdhp-repository"
 
-
 if [ -d ${LOCAL_DIR_PATH} ]
 then
-    rsync -a -v -e ssh --delete ${LOCAL_DIR_PATH}/ ${REMOTE_DIR_PATH}/
+    #rsync -a -v -e ssh --delete ${LOCAL_DIR_PATH}/ ${REMOTE_DIR_PATH}/
+    rsync -a -v -e ssh ${LOCAL_DIR_PATH}/ ${REMOTE_DIR_PATH}/
 else
     echo "ERROR: ${LOCAL_DIR_PATH} doesn't exist!"
+    exit 1
 fi
 
